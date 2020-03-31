9ac929dd6a2ee011af69ec018ecf7e0691bb6e19
Bryan Torres <bryantorresckp@gmail.com>
2020-03-31 16:55:47 -0500

    Creando servicio para obtener categorias

--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+const URL = environment.urlTurismoApi;
 
 @Injectable({
   providedIn: 'root'
 })
+
+
+
 export class CategoriaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCategorias(){
+    return this.http.get(`${ URL }/categoria/`);
+  }
 }
