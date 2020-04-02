b7d9346cde0a250121db4fdabf0e458fdd115a62
Bryan Torres <bryantorresckp@gmail.com>
2020-04-02 17:32:14 -0500

    Agregando imagen de fondo en categorias

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CategoriaService } from 'src/app/services/categoria.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  categories: any[] = [];
+
+  constructor(private categoriaService: CategoriaService) {
+    this.categoriaService.getCategorias().subscribe( resp => {
+      // console.log(resp);
+      this.categories = resp['categorias'];
+      console.log(this.categories);
+    });
+  }
 
 }
