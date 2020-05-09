1590e00c50401f5a1ea631f5ccd5a73a7590175b
Bryan Torres <bryantorresckp@gmail.com>
2020-05-08 22:03:06 -0500

    Creando marcadores prsonalizados en mapbox

--- src/app/pages/tab3/tab3.page.ts
+++ src/app/pages/tab3/tab3.page.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import mapboxgl from 'mapbox-gl';
 import { environment } from 'src/environments/environment';
+import { SitioService } from 'src/app/services/sitio.service';
+import { Sitio } from 'src/app/interfaces/sitio.interface';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-tab3',
@@ -11,85 +14,132 @@ import { environment } from 'src/environments/environment';
 export class Tab3Page {
 
   mapboxToken = environment.mapboxToken;
+  sitios: Sitio[];
+  map: mapboxgl.Map;
 
-  constructor(private geolocation: Geolocation) {}
+  constructor(private geolocation: Geolocation, private sitioService: SitioService) { }
 
-  ionViewDidEnter() { this.mapboxMap(); }
+  ionViewDidEnter() { this.cargarDatos(); }
+
+  private cargarDatos() {
+    this.sitioService.getSitios().subscribe(data => {
+      this.sitios = data['sitios'];
+    }, error => {
+      console.error(error)
+    }, () => this.mapboxMap())
+  }
 
 
   private mapboxMap() {
     mapboxgl.accessToken = this.mapboxToken;
-    let map = new mapboxgl.Map({
-      style: 'mapbox://styles/mapbox/light-v10',
-      center: [-78.6269361, -1.2412194],
-      zoom: 15.5,
-      pitch: 45,
-      bearing: -17.6,
-      container: 'map',
-      antialias: true
-    });
-    // The 'building' layer in the mapbox-streets vector source contains building-height
-    // data from OpenStreetMap.
-    map.on('load', () => {
-      map.resize();
-      // Insert the layer beneath any symbol layer.
-      let layers = map.getStyle().layers;
-      let labelLayerId;
-      for (let i = 0; i < layers.length; i++) {
-        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
-          labelLayerId = layers[i].id;
-          break;
+    if( this.map === undefined) {
+      this.map = new mapboxgl.Map({
+        style: 'mapbox://styles/mapbox/light-v10',
+        center: [-78.6269361, -1.2412194],
+        //center: [-63.29223632812499, -18.28151823530889],
+        zoom: 15.5,
+        pitch: 45,
+        bearing: -17.6,
+        container: 'map',
+        antialias: true
+      });
+      // The 'building' layer in the mapbox-streets vector source contains building-height
+      // data from OpenStreetMap.
+      this.map.on('load', () => {
+        this.map.resize();
+        // Insert the layer beneath any symbol layer.
+        let layers = this.map.getStyle().layers;
+        let labelLayerId;
+        for (let i = 0; i < layers.length; i++) {
+          if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+            labelLayerId = layers[i].id;
+            break;
+          }
         }
-      }
-      map.addLayer({
-        'id': '3d-buildings',
-        'source': 'composite',
-        'source-layer': 'building',
-        'filter': ['==', 'extrude', 'true'],
-        'type': 'fill-extrusion',
-        'minzoom': 15,
-        'paint': {
-          'fill-extrusion-color': '#aaa',
-          // use an 'interpolate' expression to add a smooth transition effect to the
-          // buildings as the user zooms in
-          'fill-extrusion-height': [
-            'interpolate',
-            ['linear'],
-            ['zoom'],
-            15,
-            0,
-            15.05,
-            ['get', 'height']
-          ],
-          'fill-extrusion-base': [
-            'interpolate',
-            ['linear'],
-            ['zoom'],
-            15,
-            0,
-            15.05,
-            ['get', 'min_height']
-          ],
-          'fill-extrusion-opacity': 0.6
-        }
-      }, labelLayerId);
-    });
-
-    let marker = new mapboxgl.Marker({
-      draggable: false
-    })
-      .setLngLat([-78.6269361, -1.2412194])
-      .addTo(map);
-
-    // Add geolocate control to the map.
-    map.addControl(
-      new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true
-        },
-        trackUserLocation: true
+        this.map.addLayer({
+          'id': '3d-buildings',
+          'source': 'composite',
+          'source-layer': 'building',
+          'filter': ['==', 'extrude', 'true'],
+          'type': 'fill-extrusion',
+          'minzoom': 15,
+          'paint': {
+            'fill-extrusion-color': '#aaa',
+            // use an 'interpolate' expression to add a smooth transition effect to the
+            // buildings as the user zooms in
+            'fill-extrusion-height': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              15,
+              0,
+              15.05,
+              ['get', 'height']
+            ],
+            'fill-extrusion-base': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              15,
+              0,
+              15.05,
+              ['get', 'min_height']
+            ],
+            'fill-extrusion-opacity': 0.6
+          }
+        }, labelLayerId);
+      });
+  
+      let marker = new mapboxgl.Marker({
+        draggable: false
       })
-    );
+        .setLngLat([-78.6269361, -1.2412194])
+        .addTo(this.map);
+  
+      // Add geolocate control to the map.
+      this.map.addControl(
+        new mapboxgl.GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true
+          },
+          trackUserLocation: true
+        })
+      );
+  
+      this.crearMarcadores();
+    }    
+   
+    
+
   }
 
+  private crearMarcadores() {
+    from(this.sitios).subscribe((sitio: Sitio) => {
+      let el = document.createElement('div');
+      el.className = 'marker';
+      el.style.backgroundImage = `url(assets/markers/${sitio.categoria.marcador})`;
+      el.style.backgroundSize = 'cover';
+      el.style.width = '50px';
+      el.style.height = '50px';
+      el.style.borderRadius = '50%';
+      el.addEventListener('click', function () {
+        window.alert('Hola');
+      });
+
+      // create the popup
+      var popup = new mapboxgl.Popup({ offset: 25 }).setText(
+        'Construction on the Washington Monument began in 1848.'
+      );
+      // add marker to map
+      new mapboxgl.Marker(el)
+        .setLngLat([sitio.longitud, sitio.latitud])
+        .addTo(this.map);
+    });
+
+  }
+
+    /** Remove map when we have multiple map object */
+    ionViewWillLeave() {
+      // this.map.remove();
+    }
 }
